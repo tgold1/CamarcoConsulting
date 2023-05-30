13a64c8e76e2901830042cca8951ff9dba4947c6
Tim Gold <timothygold@comcast.net>
2023-05-30 13:25:41 -0400

    added a const signup to signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,3 +5,13 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+
+const Signup = () => {
+    const [formState, setFormState] = useState ({
+        username: '',
+        email: '',
+        password: '',
+    });
+    const [addUser, {error, data}] = useMutation(ADD_USER);
+    
+}
