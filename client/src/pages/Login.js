2a18417f4d0aea361c6548f53ba8c908fa1826c2
Tim Gold <timothygold@comcast.net>
2023-05-30 12:03:48 -0400

    added to login.js with const login an const handleChange

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,3 +4,18 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    const [login, {error, data}] = useMutation(LOGIN_USER);
+
+}
+
+    const handleChange = (event ) => {
+        const {name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
