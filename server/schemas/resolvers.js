634ec845eac20b58ad94cb861188c0fcc8387519
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-30 14:25:39 -0400

    added logic for adding a project

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,39 +5,51 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         Projects: async () => {
-            return await user.findOne({_id}).populate('projects')
+            return await user.findOne({ _id }).populate('projects')
         },
-        Invoices: async ()=>{
-            return await User.findOne({_id}).populate('invoices')
+        Invoices: async () => {
+            return await User.findOne({ _id }).populate('invoices')
         },
-        Users: async ()=>{
+        Users: async () => {
             return await User.find({})
         }
     },
 
-    Mutation:{
+    Mutation: {
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
-          },
-          login: async (parent, { email, password }) => {
+        },
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-      
+
             if (!user) {
-              throw new AuthenticationError('No user found with this email address');
+                throw new AuthenticationError('No user found with this email address');
             }
-      
+
             const correctPw = await user.isCorrectPassword(password);
-      
+
             if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError('Incorrect credentials');
             }
-      
+
             const token = signToken(user);
-      
+
             return { token, user };
-          },
+        },
+        addProject: async (parent,{Project.body}, context) => {
+            if (context.user){
+                const project = await Project.create({
+                    project.body,
+                });
+                await User.findOneAndUpdate(
+                    {_id:context.user._id},
+                    {$addToSet:{projects: project._id}}
+                )
+            }
+        }
+
     }
 };
 
