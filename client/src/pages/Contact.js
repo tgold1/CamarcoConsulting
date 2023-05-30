8a78f1191e4e00e80acd4039228a63c1d9e6abce
Tim Gold <timothygold@comcast.net>
2023-05-30 14:06:20 -0400

    added a const ContactForm to contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,5 +6,11 @@ import { ADD_CONTACT} from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
 
+const ContactForm = () => {
+    const [contactMessage, setContactMessag] = useState('');
+    const [contactName, setContactName] = useState('');
+    const [contactEmail, setContactEmail] = useState('');
 
+   
+}
 
