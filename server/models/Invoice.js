b5ad1d1ea8fc64be34bdd601898e1e7b8e94c434
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-05-30 11:55:23 -0400

    pulling tims changes, added project.js and invoice.js

---
+++ server/models/Invoice.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const invoiceSchema = new Schema({
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Invoice = model('Invoice', invoiceSchema);
+
+module.exports = Invoice;
