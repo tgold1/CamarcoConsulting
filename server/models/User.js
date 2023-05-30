654cf02f06a33b62eb3881a69b7ff915c25cbc0c
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-30 11:49:22 -0400

    started index in model and export code in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,21 @@ const bcrypt = require('bcrypt');
 const userSchema=new Schema({
     userName:{
         type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    userRole:{
+        type: Boolean,
         required: true
     }
 })
+
+const User = model('User', userSchema);
+  
+  module.exports = User;
