fbf4b56d3beb72cad812a4c132057b676832817a
Tim Gold <timothygold@comcast.net>
2023-05-31 14:19:52 -0400

    added to the links in header in index.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -38,17 +38,17 @@ const Header = () => {
           )}
         </div> 
         <div>
-          <Link className="text-light" to="/">
+          <Link className="text-light" to="/contact">
             <h1 className="m-0">Contact</h1>
           </Link>
         </div>
         <div>
-          <Link className="text-light" to="/">
+          <Link className="text-light" to="/services">
             <h1 className="m-0">Services</h1>
           </Link>
         </div>
         <div>
-          <Link className="text-light" to="/">
+          <Link className="text-light" to="/casestudies">
             <h1 className="m-0">Casestudies</h1>
           </Link>
         </div>
@@ -57,43 +57,3 @@ const Header = () => {
     )
 }
 
-// export default function PortfolioContainer() {
-//     const [currentPage, setCurrentPage] = useState('Aboutme');
-    
-//     // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-//     const renderPage = () => {
-      
-//       if (currentPage === 'Resume') {
-//         return <Resume />;
-//       }
-//       if (currentPage === 'Aboutme') {
-//         return <Aboutme />;
-//       }
-//       if (currentPage === 'Portfolio') {
-//         return <Portfolio />;
-//       }
-//       return <Contact />;
-    
-//     };
-//     const renderFooter = () => {
-//       return Footer
-//     }
-  
-//     const renderHeader = () => {
-//       return Header
-//     }
-//     const handlePageChange = (page) => setCurrentPage(page);
-  
-//     return (
-//       <div>
-//         {/* We are passing the currentPage from state and the function to update it */}
-//         <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-//         <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-//         <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
-//         {/* Here we are calling the renderPage method which will return a component  */}
-//         {renderPage()}{renderHeader()}
-//         {renderFooter()}
-//       </div>
-//     );
-   
-//   }
