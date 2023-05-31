23a538c11efe8528090d730e7e462760b46a2d5f
Tim Gold <timothygold@comcast.net>
2023-05-31 11:07:22 -0400

    added const client to app.js in the client folder

--- client/src/App.js
+++ client/src/App.js
@@ -34,6 +34,12 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 
 
 function App() {
