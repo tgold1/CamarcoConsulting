07631ea0d1cbb867112b48fd9c129f6a0b31449b
Tim Gold <timothygold@comcast.net>
2023-05-31 11:01:37 -0400

    added const httpLink and authLink to app.js in the client folder

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,21 @@ import Casestudies from './pages/Casestudies';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+const httpLink = createHttplink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
 
 function App() {
   return (
