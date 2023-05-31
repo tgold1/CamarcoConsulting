31f4e9256f7ab4fa7afbd83d6353953dd30d7289
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-31 11:19:52 -0400

    added logic for adding an invoice

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ type Mutation{
     addUser(username: String!, email: String!, password: String!): Auth
     login( email: String!, password: String!): Auth
     addProject(title: String!, description: String!, startDate: Date!, endDate: Date!): Project
-    addInvoice()
+    addInvoice(project: , amount: Number!, currency: String!, dueDate: Date!, paid: Boolean!,  ): Invoice
 }
 `
 
