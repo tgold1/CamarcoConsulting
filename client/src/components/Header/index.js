3e865599d6c8b647b96d4ea8e94b924b6eb98314
Tim Gold <timothygold@comcast.net>
2023-05-31 13:37:09 -0400

    added a const Header in the index.js in the Header folder

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,3 +3,10 @@ import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
 
+const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+    
+}
