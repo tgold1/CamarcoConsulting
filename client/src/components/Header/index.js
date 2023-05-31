3a63aef9765225cb33e9948b99c61c6eb7b02514
Tim Gold <timothygold@comcast.net>
2023-05-31 13:49:07 -0400

    added a header return in the const Header in the header folder in the index.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,5 +8,15 @@ const Header = () => {
         event.preventDefault();
         Auth.logout();
     };
-    
+    return (
+        <header className='bg-primary text-light mb-4 py-3 flex-row align-center'>
+            <div className='container flex-row justify-space-between-lg justify-center align-center'>
+            <div>
+          <Link className="text-light" to="/">
+            <h1 className="m-0">Home</h1>
+          </Link>
+        </div>
+
+            </div>
+    )
 }
