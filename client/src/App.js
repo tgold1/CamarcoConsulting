071ce940c98ccb4e660a968a502d4a7c7992d017
Tim Gold <timothygold@comcast.net>
2023-05-31 10:37:20 -0400

    added imports to app.js to connect to react router and apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttplink, 
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
