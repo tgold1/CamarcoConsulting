793a81298da8353dbe231ebfb14c5b7a9c91ee85
Tim Gold <timothygold@comcast.net>
2023-05-31 10:47:58 -0400

    added imports for pages to app.js in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,16 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Casestudies from './pages/Casestudies';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
