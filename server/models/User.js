b9cd6c18eb25a2e98828f46128b7e51f51e26d4c
Tim Gold <timothygold@comcast.net>
2023-06-01 14:11:09 -0400

    added in user.js bug fix

--- server/models/User.js
+++ server/models/User.js
@@ -35,7 +35,10 @@ const userSchema=new Schema({
         ref:'Invoice'
     }
 
-})
+});
+userSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
 
 const User = model('User', userSchema);
   
