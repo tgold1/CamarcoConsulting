d77a9113e6ab273a8e621227c647abb5f312603a
CrowTrooper202 <adamb88cke@gmail.com>
2023-06-04 18:28:16 -0400

    started project list component

---
+++ client/src/components/ProjectList/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProjectList = ({
+    Projects,
+    ShowCustomer = true,
+
+}) => {if (!Projects.length) {
+    return <h3>No Projects listed</h3>
+}
+return(
+    
+)
+}
--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const UserList = ({
-    Users,
-    Company,
-}) => {
-    if (!Users.length) {
-        return <h3>No Users</h3>
-    }
+// const UserList = ({
+//     Users,
+//     Company,
+// }) => {
+//     if (!Users.length) {
+//         return <h3>No Users</h3>
+//     }
 
-    return (
-        <div>
-          {Users && User.map((User)=>(
-            <div key={User._id} className="card md-3">
+//     return (
+//         <div>
+//           {Users && User.map((User)=>(
+//             <div key={User._id} className="card md-3">
                 
-            </div>
-          ))}  
+//             </div>
+//           ))}  
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default UserList;
+// export default UserList;
